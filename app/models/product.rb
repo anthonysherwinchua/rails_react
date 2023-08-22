71f11cb57239bb5a5700d14bec1cc7e34a8012f6
anthonysherwinchua <anthonysherwinchua@gmail.com>
2023-08-22 12:57:18 +0800

    add scopes to Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,7 @@ class Product < ApplicationRecord
   validates :name, presence: true, uniqueness: { scope: :category_id }
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :sale_price, numericality: { less_than: :price }, allow_blank: true
+
+  scope :regular, -> { where(sale_price: nil) }
+  scope :on_sale, -> { where.not(sale_price: nil) }
 end
