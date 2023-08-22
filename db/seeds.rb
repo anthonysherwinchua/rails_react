bfffbd0ab60e9d6bb4a94ba1fffa330bd699ddc4
anthonysherwinchua <anthonysherwinchua@gmail.com>
2023-08-22 12:57:26 +0800

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'factory_bot_rails'
+
+categories = FactoryBot.create_list(:category, 10)
+
+categories.each do |category|
+  1.upto([*0..100].sample).each do |i|
+    price = [*10..1000].sample
+    sale_price = [price * 0.25, price * 0.5, price * 0.75, price * 0.90, nil].sample
+
+    FactoryBot.create(:product, category: category, price: price, sale_price: sale_price)
+  end
+end
