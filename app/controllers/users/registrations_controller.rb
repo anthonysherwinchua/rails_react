bc2b92225127105b9dc086e5f7347b9cb5afac8d
anthonysherwinchua <anthonysherwinchua@gmail.com>
2023-09-05 11:05:39 +0800

    add serialized responses

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,4 +2,14 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   respond_to :json
+
+  private
+
+  def respond_with(current_user, _opts = {})
+    if resource.persisted?
+      render json: current_user, :status: :ok
+    else
+      render json: current_user.errors, status: :unprocessable_entity
+    end
+  end
 end
