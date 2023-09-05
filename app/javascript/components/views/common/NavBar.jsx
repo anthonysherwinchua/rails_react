fa8328057777bbab2d1c7db19591b102498d581d
anthonysherwinchua <anthonysherwinchua@gmail.com>
2023-09-05 19:25:45 +0800

    fix logout

--- app/javascript/components/views/common/NavBar.jsx
+++ app/javascript/components/views/common/NavBar.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import NavLink from "./NavLink"
 import Confirm from './Confirm';
 import UserProfile from './UserProfile';
 
 const NavBar = () => {
+  const navigate = useNavigate();
   const [user, setUser] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
 
@@ -36,6 +37,7 @@ const NavBar = () => {
       })
       .finally(() => {
         UserProfile.removeUser()
+        navigate(`/`);
       });
   };
 
