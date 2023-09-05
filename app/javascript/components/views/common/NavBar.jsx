6d2f5e66b484937d5958b2fb5d46f0a0fb08af20
anthonysherwinchua <anthonysherwinchua@gmail.com>
2023-09-05 14:47:14 +0800

    add user logout

--- app/javascript/components/views/common/NavBar.jsx
+++ app/javascript/components/views/common/NavBar.jsx
@@ -1,7 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import NavLink from "./NavLink"
+import Confirm from './Confirm';
+import UserProfile from './UserProfile';
 
 const NavBar = () => {
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const logout = () => {
+    const url = `/api/logout`;
+    const token = document.querySelector('meta[name="csrf-token"]').content;
+
+    fetch(url, {
+      method: "DELETE",
+      headers: {
+        "X-CSRF-Token": token,
+        "Content-Type": "application/json",
+      },
+    })
+      .then(res => {
+        handleResponse(res, (r) => {
+          if (r.status == 'error') {
+            const errorMessages = r.data.map((message, key) => (
+              <Error key={key} message={message} />
+            ));
+            setErrorMessage(errorMessages);
+          } else {
+            navigate(`/`);
+          }
+        })
+      })
+      .catch((e) => {
+        setErrorMessage('Something went wrong. <br/>Error Message: ' + e);
+      })
+      .finally(() => {
+        UserProfile.setId('')
+        UserProfile.setName('')
+      });
+  };
+
+  let sessionNavLink = <NavLink path="/signup" text="Sign Up" />
+
+  const id = UserProfile.getId()
+
+  if (UserProfile.getName()) {
+    sessionNavLink = (
+      <>
+        <Link className="btn btn-danger" data-bs-toggle="modal" data-bs-target="#confirmModal">
+          Logout
+        </Link>
+        <Confirm modalID="confirmModal" title={"Log out"} message="Are you sure?" confirm="Logout!" cancel="No" onConfirm={logout} />
+      </>
+    )
+  }
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light mb-3" style={{ backgroundColor: '#e3f2fd' }}>
       <div className="container-fluid">
@@ -12,6 +64,9 @@ const NavBar = () => {
             </li>
             <li className="nav-item">
               <NavLink path="/categories" text="Categories" />
+            </li>
+            <li className="nav-item">
+              {sessionNavLink}
             </li>
           </ul>
         </div>
