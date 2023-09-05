4430ea6c714e760189d91281337c619c0f22593b
anthonysherwinchua <anthonysherwinchua@gmail.com>
2023-09-05 16:39:19 +0800

    improved navbar showing user session

--- app/javascript/components/views/common/NavBar.jsx
+++ app/javascript/components/views/common/NavBar.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import NavLink from "./NavLink"
 import Confirm from './Confirm';
 import UserProfile from './UserProfile';
 
 const NavBar = () => {
+  const [user, setUser] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
 
   const logout = () => {
@@ -34,31 +35,37 @@ const NavBar = () => {
         setErrorMessage('Something went wrong. <br/>Error Message: ' + e);
       })
       .finally(() => {
-        UserProfile.setUser('')
+        UserProfile.removeUser()
       });
   };
 
-  let sessionNavLink = (
-    <>
-      <li className="nav-item"><NavLink path="/signup" text="Sign Up" /></li>
-      <li className="nav-item"><NavLink path="/login" text="Login" /></li>
-    </>
-  )
+  let sessionNavLink
 
-  const id = UserProfile.getId()
+  useEffect(() => {
+    setUser(UserProfile.getUser())
+  }, []);
 
-  if (UserProfile.getName()) {
+  if (user['name'] === undefined) {
     sessionNavLink = (
       <>
-        <li className="nav-item">
-          <Link className="btn btn-danger" data-bs-toggle="modal" data-bs-target="#confirmModal">
-            Logout
-          </Link>
-          <Confirm modalID="confirmModal" title={"Log out"} message="Are you sure?" confirm="Logout!" cancel="No" onConfirm={logout} />
-        </li>
+        <li className="nav-item"><NavLink path="/signup" text="Sign Up" /></li>
+        <li className="nav-item"><NavLink path="/login" text="Login" /></li>
       </>
     )
+  } else {
+    sessionNavLink = (
+      <li className="nav-item">
+        <Link className="btn btn-danger" data-bs-toggle="modal" data-bs-target="#confirmModal">
+          Logout
+        </Link>
+        <Confirm modalID="confirmModal" title={"Log out"} message="Are you sure?" confirm="Logout!" cancel="No" onConfirm={logout} />
+      </li>
+    )
   }
+
+  window.addEventListener('storage', () => {
+    setUser(UserProfile.getUser())
+  })
 
   return (
     <nav className="navbar navbar-expand-lg navbar-light mb-3" style={{ backgroundColor: '#e3f2fd' }}>
--- app/javascript/components/views/common/UserProfile.jsx
+++ app/javascript/components/views/common/UserProfile.jsx
@@ -1,31 +1,39 @@
 var UserProfile = (function () {
   var getName = function () {
-    return getUserFromLocalStorage()['name']
+    return getUser()['name']
   };
 
   var getId = function () {
-    return getUserFromLocalStorage()['id']
+    return getUser()['id']
   };
 
   var getJTI = function () {
-    return getUserFromLocalStorage()['jti']
+    return getUser()['jti']
   };
 
   var setUser = function (user) {
     localStorage.setItem('user', JSON.stringify(user))
+    window.dispatchEvent(new Event("storage"));
   }
 
-  function getUserFromLocalStorage() {
+  var removeUser = function () {
+    localStorage.removeItem('user')
+    window.dispatchEvent(new Event("storage"));
+  }
+
+  var getUser = function () {
     let userProfile = localStorage.getItem('user') || '{}'
 
     return JSON.parse(userProfile)
   }
 
   return {
+    getUser,
     getName,
     getId,
     getJTI,
     setUser,
+    removeUser,
   }
 
 })();
