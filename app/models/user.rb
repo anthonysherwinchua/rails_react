ff992e6479f84898ab2e3919515bf56af615e220
anthonysherwinchua <anthonysherwinchua@gmail.com>
2023-09-06 16:08:05 +0800

    pair programming

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
+
+  before_create :generate_otp_secret
+
+  private
+
+  def generate_otp_secret
+    self.otp_secret = SecureRandom.hex(16)
+  end
 end
